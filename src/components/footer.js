65e608db28254fb054c3391df47b381943ecb25e
uvc <uvc@uvcdc.com>
2022-09-07 16:56:43 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from 'uswds/img/logo-img.png';
+import logo from 'images/United_States_Department_of_Defense_Seal.svg.png';
 
 const Footer = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
@@ -8,24 +8,35 @@ const Footer = () => (
         <div className="grid-row padding-3">
           <div className="tablet:grid-col">
             <div className="logo-links">
-              <a className="footer-logo media_link" href="https://18f.gsa.gov">
+              <a className="footer-logo media_link" href="https://web.dma.mil">
                 <img src={logo} width="50" alt="Agency logo" />
               </a>
             </div>
           </div>
-          <div className="tablet:grid-col"></div>
           <div className="tablet:grid-col">
-            <ul className="add-list-reset">
-              <li>Footer link</li>
-              <li>Footer link</li>
-              <li>Footer link</li>
+          <ul className="add-list-reset">
+              <li>About DoD</li>
+              <li> No Fear</li>
+              <li>USA.gov
+              <li> Privacy and Security</li>
+              <li></li>EEO</li>
+            </ul>
+          </div>
+          <div className="tablet:grid-col">
+          <ul className="add-list-reset">
+              <li>Contact Us</li>
+              <li>Plain Language</li>
+              <li>FOIA</li>
+              <li>DOD IG</li>
+              <li>DOD Jobs</li>
             </ul>
           </div>
           <div className="tablet:grid-col">
             <ul className="add-list-reset">
-              <li>Footer link</li>
-              <li>Footer link</li>
-              <li>Footer link</li>
+              <li>Small Business Office</li>
+              <li>Accessibility</li>
+              <li>Link Disclaimer</li>
+              <li>DOD Web Policy</li>
             </ul>
           </div>
         </div>
