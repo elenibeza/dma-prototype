ab4cbaffbafbd03f810d822ccce6c7a51dfc4b00
uvc <uvc@uvcdc.com>
2022-09-07 17:25:52 -0400

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -9,15 +9,15 @@ const Hero = () => (
     <div className="grid-container">
       <div className="usa-hero__callout">
         <h2 className="usa-hero__heading">
-          <span className="usa-hero__heading--alt">Hero callout:</span>Bring
-          attention to a project priority
+          <span className="usa-hero__heading--alt">News:</span>DOD Releases First Departmentwide Social Media Policy 
         </h2>
         <p>
-          Support the callout with some short explanatory text. You don’t need
-          more than a couple of sentences.
+        The Defense Department released a policy about how DOD should use official social media 
+        accounts to best advance the mission of 
+        the U.S. military and further instill trust in the credibility of the DOD 
         </p>
-        <a className="usa-button" href="/">
-          Call to action
+        <a className="usa-button" href="https://www.web.dma.mil/News/Article/3131081/dod-releases-first-departmentwide-social-media-policy/">
+          Read More
         </a>
       </div>
     </div>
