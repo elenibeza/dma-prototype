ff95d61fa981b78af3e41ad01075623903a61b06
uvc <uvc@uvcdc.com>
2022-09-07 16:10:02 -0400

    Update tagline.js

--- src/components/tagline.js
+++ src/components/tagline.js
@@ -18,7 +18,9 @@ const Tagline = () => (
           enterprise hosting solution and robust distribution system that meets all 
           Department of Defense standards for information security. This solution is built for the Public Affairs mission in mind to professionally create web sites for public viewing. Customers enjoy the user experience with our content management system: Value, Ease of Use, and Enjoyment to Use. Check out our "Testimonials" page to see what our customers had to say about us!
         </p>
-    
+       <p>
+       Web Enterprise Business (WEB.mil) provides customer organizations with a website content management system (CMS) and hosting solution referred to as AFPIMS, built with the Public Affairs (PA) mission in mind. The WEB.mil CMS is not meant to be an online publications library or document management system. It may not be feasible for our customers to use the CMS for this purpose.
+        </p> 
       </div>
     </div>
   </section>
