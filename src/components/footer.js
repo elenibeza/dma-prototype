432818c4b23ca4fa70ba7b71d355b616c9026091
uvc <uvc@uvcdc.com>
2022-09-07 22:08:05 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -44,6 +44,7 @@ const Footer = () => (
     </div>
     
 
+    
     <div className="footer-section-bottom bg-base-lighter">
      <p style="align:center">Hosted by Defense Media Activity - WEB.mil </p>
     </div>
