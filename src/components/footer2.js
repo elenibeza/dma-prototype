4147a364041736e5473a7e6b593869655be34222
uvc <uvc@uvcdc.com>
2022-09-08 09:01:21 -0400

    copyright added

--- src/components/footer2.js
+++ src/components/footer2.js
@@ -3,8 +3,8 @@ import logo from '../images/United_States_Department_of_Defense_Seal.svg.png';
 
 const Footer2 = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
-    <div className="footer-section-bottom bg-base-lighter">
- 
+    <div className="footer2">
+    Hosted by Defense Media Activity - WEB.mil  
     </div>
 
   </footer>
