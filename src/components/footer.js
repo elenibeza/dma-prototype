ab3e7f005ff68a9d0cf49b1df423bd4b080a90c3
uvc <uvc@uvcdc.com>
2022-09-08 08:44:24 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from '../images/United_States_Department_of_Defense_Seal.svg.png';
-import customcss from '';
+import logo from '../images/United_States_Department_of_Defense_Seal.png';
+
 
 const Footer = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
