f6aa6448940cf8d4a01c9a874635ed36ab61d4ba
uvc <uvc@uvcdc.com>
2022-09-14 22:04:45 -0400

    Update test.js

--- src/templates/test.js
+++ src/templates/test.js
@@ -1,10 +1,10 @@
-import React from "react"
-import "froala-editor/js/plugins.pkgd.min.js";
-import "froala-editor/css/froala_style.min.css";;
-import FroalaEditorComponent from "react-froala-wysiwyg";
+// import React from "react"
+// import "froala-editor/js/plugins.pkgd.min.js";
+// import "froala-editor/css/froala_style.min.css";;
+// import FroalaEditorComponent from "react-froala-wysiwyg";
 
 
-function App(){
-    return < FroalaEditorComponent tag="textarea" />; 
-}
-export default App;
+// function App(){
+//     return < FroalaEditorComponent tag="textarea" />; 
+// }
+// export default App;
