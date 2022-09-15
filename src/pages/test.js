43d51f6344cd60c7a104909e1dfe625ff6e1b882
uvc <uvc@uvcdc.com>
2022-09-14 22:04:18 -0400

    Revert "Update test.js"
    This reverts commit 7c23300f4e0fc43918d94f6267014fce09a15a5f.

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,13 +1,13 @@
-// import React from 'react';
+import React from 'react';
 
-// import Layout from '../components/layout';
-// import SEO from '../components/seo';
-// import Hero from '../components/hero';
-// import Tagline from '../components/tagline';
-// import Highlights from '../components/highlights';
-// import FroalaEditorComponent from "react-froala-wysiwyg";
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Hero from '../components/hero';
+import Tagline from '../components/tagline';
+import Highlights from '../components/highlights';
+import FroalaEditorComponent from "react-froala-wysiwyg";
 
-// function App(){
-//     return < FroalaEditorComponent tag="textarea" />; 
-//   }
-// export default App;
+function App(){
+    return < FroalaEditorComponent tag="textarea" />; 
+  }
+export default App;
